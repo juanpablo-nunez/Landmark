5c80e7d94ece88f311195511defa6ed8126a1e48
Juan Pablo Nuñez <juanpanm2409@gmail.com>
2024-05-11 15:22:51 -0400

    Initial Commit

---
+++ posts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  posts
+//
+//  Created by Juan Pablo Nuñez Molina on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ posts/postsApp.swift
@@ -0,0 +1,17 @@
+//
+//  postsApp.swift
+//  posts
+//
+//  Created by Juan Pablo Nuñez Molina on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct postsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
